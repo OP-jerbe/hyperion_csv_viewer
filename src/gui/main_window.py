d390d96664b83a3ea71019a25a7b8b5a8f88c719
OP-jerbe <j.erbe@oregon-physics.com>
2025-04-11 21:31:27 -0700

    Creates a main gui window with qt-material styling

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -1,8 +1,31 @@
+import sys
+
 from PySide6.QtWidgets import QMainWindow
+from PySide6.QtGui import QIcon
+from qt_material import apply_stylesheet
 
 
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
-        self.setWindowTitle('My Time Series Project')
-        self.setGeometry(100, 100, 800, 600)
+        self.installEventFilter(self)
+        self.create_gui()
+
+    def create_gui(self) -> None:
+        window_width = 470
+        window_height = 275
+        button_width = 100
+        button_height = 40
+        dropdown_width = 100
+        dropdown_height = 40
+        self.setFixedSize(window_width, window_height)
+        self.setWindowTitle('Hyperion Test Data Viewer')
+        if hasattr(sys, 'frozen'):  # Check if running from the Pyinstaller EXE
+            icon_path = sys._MEIPASS + '/assets/icon.ico'  # type: ignore
+        else:
+            icon_path = './assets/icon.ico'  # Use the local icon file in dev mode
+        self.setWindowIcon(QIcon(icon_path))
+        apply_stylesheet(self, theme='dark_lightgreen.xml', invert_secondary=True)
+        self.setStyleSheet(
+            self.styleSheet() + """QLineEdit, QTextEdit {color: lightgreen;}"""
+        )
