4276886bfda02fe57637dff33e9ade8d4e1af6d0
OP-jerbe <j.erbe@oregon-physics.com>
2025-04-11 21:51:12 -0700

    Adds the menu bar and options

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -1,7 +1,16 @@
 import sys
 
-from PySide6.QtWidgets import QMainWindow
-from PySide6.QtGui import QIcon
+from PySide6.QtGui import QIcon, QAction
+from PySide6.QtWidgets import (
+    QComboBox,
+    QGridLayout,
+    QHBoxLayout,
+    QLabel,
+    QLineEdit,
+    QMainWindow,
+    QPushButton,
+    QVBoxLayout,
+)
 from qt_material import apply_stylesheet
 
 
@@ -29,3 +38,20 @@ class MainWindow(QMainWindow):
         self.setStyleSheet(
             self.styleSheet() + """QLineEdit, QTextEdit {color: lightgreen;}"""
         )
+        # Create the menu bar
+        self.menu_bar = self.menuBar()
+
+        # Create the menu bar items
+        self.file_menu = self.menu_bar.addMenu('File')
+        self.save_menu = self.menu_bar.addMenu('Save')
+        self.help_menu = self.menu_bar.addMenu('Help')
+
+        # Create the QAction objects for the menus
+        self.exit_option = QAction('Exit', self)
+        self.save_3D_surface_option = QAction('Save Plot as HTML', self)
+        self.open_quick_start_guide = QAction('Quick Start Guide', self)
+
+        # Add the action objects to the menu bar items
+        self.file_menu.addAction(self.exit_option)
+        self.save_menu.addAction(self.save_3D_surface_option)
+        self.help_menu.addAction(self.open_quick_start_guide)
