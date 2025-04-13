4fcd01b5ff947e3ffcaa2436990f4d41f6f96066
OP-jerbe <j.erbe@oregon-physics.com>
2025-04-12 22:06:25 -0700

    Adds type hinting and puts combo boxes in a list called combo_boxes

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -11,6 +11,8 @@ from PySide6.QtWidgets import (
     QSizePolicy,
     QVBoxLayout,
     QWidget,
+    QMenuBar,
+    QMenu,
 )
 from qt_material import apply_stylesheet
 
@@ -44,17 +46,17 @@ class MainWindow(QMainWindow):
         )
 
         # Create the menu bar
-        self.menu_bar = self.menuBar()
+        self.menu_bar: QMenuBar = self.menuBar()
 
         # Create the menu bar items
-        self.file_menu = self.menu_bar.addMenu('File')
-        self.save_menu = self.menu_bar.addMenu('Save')
-        self.help_menu = self.menu_bar.addMenu('Help')
+        self.file_menu: QMenu = self.menu_bar.addMenu('File')
+        self.save_menu: QMenu = self.menu_bar.addMenu('Save')
+        self.help_menu: QMenu = self.menu_bar.addMenu('Help')
 
         # Create the QAction objects for the menus
-        self.exit_option = QAction('Exit', self)
-        self.save_3D_surface_option = QAction('Save Plot as HTML', self)
-        self.open_quick_start_guide = QAction('Quick Start Guide', self)
+        self.exit_option: QAction = QAction('Exit', self)
+        self.save_3D_surface_option: QAction = QAction('Save Plot as HTML', self)
+        self.open_quick_start_guide: QAction = QAction('Quick Start Guide', self)
 
         # Add the action objects to the menu bar items
         self.file_menu.addAction(self.exit_option)
@@ -62,49 +64,55 @@ class MainWindow(QMainWindow):
         self.help_menu.addAction(self.open_quick_start_guide)
 
         # Create title label and input box
-        self.title_label = QLabel('Title:')
+        self.title_label: QLabel = QLabel('Title:')
         self.title_label.setContentsMargins(0, 0, 5, 0)
         self.title_label.setSizePolicy(
             QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Preferred
         )
         self.title_label.setStyleSheet('font-size: 16pt;')
-        self.title_input = QLineEdit()
+        self.title_input: QLineEdit = QLineEdit()
         self.title_input.setFixedHeight(40)
 
         # Create the plot labels
-        self.plot1_label = QLabel('Plot 1')
+        self.plot1_label: QLabel = QLabel('Plot 1')
         self.plot1_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        self.plot2_label = QLabel('Plot 2')
+        self.plot2_label: QLabel = QLabel('Plot 2')
         self.plot2_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        self.plot3_label = QLabel('Plot 3')
+        self.plot3_label: QLabel = QLabel('Plot 3')
         self.plot3_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        self.plot4_label = QLabel('Plot 4')
+        self.plot4_label: QLabel = QLabel('Plot 4')
         self.plot4_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
 
         # Create the plot combo boxes
-        self.plot1_combo = ComboBox()
-        self.plot2_combo = ComboBox()
-        self.plot3_combo = ComboBox()
-        self.plot4_combo = ComboBox()
+        self.plot1_combo: ComboBox = ComboBox()
+        self.plot2_combo: ComboBox = ComboBox()
+        self.plot3_combo: ComboBox = ComboBox()
+        self.plot4_combo: ComboBox = ComboBox()
+        self.combo_boxes: list[ComboBox] = [
+            self.plot1_combo,
+            self.plot2_combo,
+            self.plot3_combo,
+            self.plot4_combo,
+        ]
 
         # Create the buttons
-        self.select_csv_button = PushButton('Select CSV Files')
-        self.plot_button = PushButton('Plot Data')
+        self.select_csv_button: PushButton = PushButton('Select CSV Files')
+        self.plot_button: PushButton = PushButton('Plot Data')
 
         # Create the canvas for displaying CSV data
-        self.canvas = Canvas()
+        self.canvas: Canvas = Canvas()
         self.canvas.display_csv_files(
             ['file1.csv', 'file2.csv', 'file3.csv']
         )  # example data for testing
 
         # Create the layout for the main window
-        self.h_title_layout = QHBoxLayout()
+        self.h_title_layout: QHBoxLayout = QHBoxLayout()
         self.h_title_layout.addWidget(self.title_label, stretch=0)
         self.h_title_layout.addWidget(self.title_input, stretch=1)
         self.h_title_layout.setContentsMargins(10, 0, 10, 0)
         self.h_title_layout.setSpacing(0)
 
-        self.g_combo_box_layout = QGridLayout()
+        self.g_combo_box_layout: QGridLayout = QGridLayout()
         self.g_combo_box_layout.addWidget(self.plot1_label, 0, 0)
         self.g_combo_box_layout.addWidget(self.plot2_label, 0, 1)
         self.g_combo_box_layout.addWidget(self.plot1_combo, 1, 0)
@@ -118,23 +126,23 @@ class MainWindow(QMainWindow):
         self.g_combo_box_layout.setContentsMargins(10, 10, 10, 0)
         self.g_combo_box_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        self.v_button_layout = QVBoxLayout()
+        self.v_button_layout: QVBoxLayout = QVBoxLayout()
         self.v_button_layout.addWidget(self.select_csv_button)
         self.v_button_layout.addWidget(self.plot_button)
         self.v_button_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.v_button_layout.setContentsMargins(0, 10, 0, 0)
 
-        self.v_canvas_layout = QVBoxLayout()
+        self.v_canvas_layout: QVBoxLayout = QVBoxLayout()
         self.v_canvas_layout.addWidget(self.canvas)
         self.v_canvas_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        self.v_main_layout = QVBoxLayout()
+        self.v_main_layout: QVBoxLayout = QVBoxLayout()
         self.v_main_layout.addLayout(self.h_title_layout)
         self.v_main_layout.addLayout(self.g_combo_box_layout)
         self.v_main_layout.addLayout(self.v_button_layout)
         self.v_main_layout.addLayout(self.v_canvas_layout)
 
-        container = QWidget()
+        container: QWidget = QWidget()
         container.setLayout(self.v_main_layout)
 
         self.setCentralWidget(container)
