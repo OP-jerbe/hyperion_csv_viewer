9b77eef39fc1c4a5bed4da13db523b584724ab40
OP-jerbe <j.erbe@oregon-physics.com>
2025-04-13 06:55:55 -0700

    Refactors to inherit from QComboBox rather than QWidget

--- src/gui/combo_box.py
+++ src/gui/combo_box.py
@@ -1,25 +1,20 @@
-from PySide6.QtWidgets import QComboBox, QVBoxLayout, QWidget
+from PySide6.QtWidgets import QComboBox
 
 
-class ComboBox(QWidget):
+class ComboBox(QComboBox):
     def __init__(self) -> None:
         super().__init__()
 
-        self.combo_box = QComboBox(self)
-        self.combo_box.setFixedHeight(40)
-        self.combo_box.setStyleSheet('color: white;')
-        self.combo_box.addItems(['None'])
-
-        layout = QVBoxLayout(self)
-        layout.addWidget(self.combo_box)
-        layout.setContentsMargins(0, 0, 0, 0)
+        self.setFixedHeight(40)
+        self.setStyleSheet('color: lightgreen;')
+        self.addItems(['None'])
 
     def populate(self, headers: list[str]) -> None:
         """Call this method to update the list of headers from the csv file."""
-        self.combo_box.clear()
-        self.combo_box.addItems(headers)
-        self.combo_box.setEditable(False)
+        self.clear()
+        self.addItems(headers)
+        self.setEditable(False)
 
     def current_text(self) -> str:
         """Returns the current text selected in the combo box."""
-        return self.combo_box.currentText()
+        return self.currentText()
